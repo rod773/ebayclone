91501803853af599f808d88f2a9b20c7fa65efdb
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-29 16:37:43 +0200

    changes topmenu

--- app/layouts/includes/TopMenu.js
+++ app/layouts/includes/TopMenu.js
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { BsChevronDown } from "react-icons/bs";
 
 export default function TopMenu() {
   return (
@@ -16,7 +17,16 @@ export default function TopMenu() {
           <Link
             href="/auth"
             className="flex items-center gap-2 hover:underline cursor-pointer"
-          ></Link>
+          >
+            <div
+              id="AuthDropdown"
+              className="absolute bg-white w-[2000px] text-[#333333] z-40
+              top-[20px] left-[0] border shadow-lg"
+            >
+              <div className="flex items-center justify-start gap-1 p-3"></div>
+            </div>
+            <BsChevronDown />
+          </Link>
         </div>
       </div>
     </>
