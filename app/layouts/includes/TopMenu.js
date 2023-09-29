e4276fcaec78d9ff13968ebf41737652647df1cb
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-29 10:21:33 +0200

    changes topmenu

--- app/layouts/includes/TopMenu.js
+++ app/layouts/includes/TopMenu.js
@@ -1,11 +1,22 @@
 "use client";
 
+import Link from "next/link";
+
 export default function TopMenu() {
   return (
     <>
-      <div id="TopMenu" class="border-b">
+      <div id="TopMenu" className="border-b">
         <div className="flex items-center justify-between w-full mx-auto max-w-[1200px]">
-          TopMenu
+          <ul
+            id="TopMenuLeft"
+            className="flex items-center text-[11px] text-[#333333] ox-2 h-8
+          "
+          ></ul>
+          <li className="relative px-3"></li>
+          <Link
+            href="/auth"
+            className="flex items-center gap-2 hover:underline cursor-pointer"
+          ></Link>
         </div>
       </div>
     </>
